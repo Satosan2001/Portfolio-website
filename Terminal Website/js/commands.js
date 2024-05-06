561ac472ac78d557a3256e9b4d1e8b2003d8c4f7
Satosan2001 <168102457+Satosan2001@users.noreply.github.com>
2024-05-06 20:15:23 +0100

    Add files via upload

---
+++ Terminal Website/js/commands.js
@@ -0,0 +1,82 @@
+var twitter = "https://www.twitter.com/";
+var password = "abir";
+var instagram = "https://www.instagram.com/salahbsth/";
+var github = "https://github.com/";
+var facebook = "https://www.facebook.com/sallah.boussettah";
+var email = 'mailto:boussettahsallah@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Salah!👋",
+"I'm a software developer, game developer, and digital artist, currently diving deep into the world of full stack development.",
+"While I'm still a student, I'm also exploring the realms of game development, constantly weighing whether to",
+"pursue a future as a game developer, a full stack developer, or perhaps both.",
+"Each project I take on is an exciting opportunity to learn and grow—whether it’s building interactive websites,",
+"or designing games just for fun. You can check out some of my projects in the other sections of this site.",
+"As I navigate through the complexities of both web and game development, I document my progress and share my",
+"experiences, hoping to inspire and guide others who are on a similar path.",
+"Join me on this adventure as I blend the art of code with creativity, building everything my heart desires,",
+"right here on this terminal website.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The question 'Who am I?' is like checking the fridge for snacks: you're not always sure what you're looking for, but you can't help looking again and again.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/ Not Yet' + '</a>',
+  'facebook       <a href="' + facebook + '" target="_blank">facebook.com/sallah.boussettah' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram.com/salahbsth' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/ Not Yet' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">hi</span>           Only for admins',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Salah?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">portfolio</span>      Open my portfolio',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index"> &copy; Salah Boussettah (SB) | All Right Reserved.</span>',
+  "##%%#######**###*#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#++*###*#*#*###**###****#*#",
+  "       /' --;^/ ,-_\\     \\ | /      ",
+  "      / / --o\\ o-\\ \\\\   --(_)--    ",
+  "     /-/-/|o|-|\\-\\\\|\\\\   / | \\       _____       __      __    ____                             __  __        __  ",
+  "      '`  ` |-|   `` '              / ___/____ _/ /___ _/ /_  / __ )____  __  _______________  / /_/ /_____ _/ /_ ",
+  "            |-|                     \\__ \\/ __ `/ / __ `/ __ \\/ __  / __ \\/ / / / ___/ ___/ _ \\/ __/ __/ __ `/ __ \\",
+  "            |-|O                    ___/ //_/ / / /_/ / / / / /_/ / /_/ / /_/ (__  |__  )  __/ /_/ /_/ /_/ / / / /         ",
+  "            |-(\\,__              /____/\\__,_/_/\\__,_/_/ /_/_____\\____/\\__,_/____/____/\\___/\\__/\\__/\\___/_//_/ /_/",
+  "         ...|-|\\--,\\_....                                                                                   © 2022",
+  "      ,;;;;;;;;;;;;;;;;;;;;;;;;,.        ",
+  "~~,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
